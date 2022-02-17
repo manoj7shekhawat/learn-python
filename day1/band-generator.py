837c235b36d434bc0cc4bda72d9a7cb707125466
manoj7shekhawat <manoj7shekhawat@gmail.com>
2022-02-17 09:51:01 +0100

    master - Added gitignore

---
+++ day1/band-generator.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+# Get city name
+city=input("Enter city name: ")
+pet=input("Enter pet name: ")
+
+print("Your band name: " + city + " " + pet)
+
+print(" Hello " + input("Your name:"))
+
+
+
