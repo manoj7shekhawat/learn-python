2cb073facdccd222482885da705e58771289e765
manoj7shekhawat <manoj7shekhawat@gmail.com>
2022-03-10 18:22:55 +0100

    master - added not guessed states to a file

--- day25/us/main.py
+++ day25/us/main.py
@@ -16,6 +16,9 @@ data = pd.read_csv("50_states.csv")
 
 total = data['state'].count()
 
+all_states = data['state'].to_list()
+print(all_states)
+
 def writeText(name, xCor, yCor):
     myT = turtle.Turtle()
     myT.penup()
@@ -24,10 +27,17 @@ def writeText(name, xCor, yCor):
     myT.color("black")
     myT.write(name, align="center", font=("Arial", 12, "normal"))
 
+guessed_states = []
 score = 0
+
 while True:
     answer = myScreen.textinput(f"Your Score {score}/{total}", "Name of a US State:").title()
+
+    if answer == "Exit":
+        break
+
     if answer in data['state'].to_list():
+        guessed_states.append(answer)
         row = data[data['state'] == answer]
         xCor = row["x"].to_list()[0]
         yCor = row["y"].to_list()[0]
@@ -39,6 +49,14 @@ while True:
 
 
 
+not_guessed_states = [x for x in all_states if (x not in guessed_states)]
 
-turtle.mainloop()
+print(not_guessed_states)
+
+with open("not_guessed_states.txt", mode="w") as fh:
+    for x in not_guessed_states:
+        fh.write(f"{x}\n")
+
+
+#turtle.mainloop()
 #myScreen.exitonclick()
