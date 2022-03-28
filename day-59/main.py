83739e9635c18bbda0cc31d09e33d793cb34628c
manoj7shekhawat <manoj7shekhawat@gmail.com>
2022-03-28 18:56:47 +0200

    master - Bootstraped blog site for day 59

--- day-59/main.py
+++ day-59/main.py
@@ -1,16 +1,25 @@
 from flask import Flask, render_template
+import requests as rq
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    response = rq.get(url="https://api.npoint.io/cd4ef3cbd01b5814a64f")
+    response.raise_for_status()
+    blogs = response.json()['blogs']
+
+    return render_template("index.html", blogs=blogs)
 
 
 @app.route("/index.html")
 def index():
-    return render_template("index.html")
+    response = rq.get(url="https://api.npoint.io/cd4ef3cbd01b5814a64f")
+    response.raise_for_status()
+    blogs = response.json()['blogs']
+
+    return render_template("index.html", blogs=blogs)
 
 
 @app.route("/about.html")
@@ -21,6 +30,20 @@ def about():
 @app.route("/contact.html")
 def contact():
     return render_template("contact.html")
+
+
+@app.route("/<int:id>")
+def post_details(id):
+
+    response = rq.get(url="https://api.npoint.io/cd4ef3cbd01b5814a64f")
+    response.raise_for_status()
+    blogs = response.json()['blogs']
+
+    for x in blogs:
+        if int(x['id']) == id:
+            blog = x
+
+    return render_template("post.html", blog=blog)
 
 
 if __name__ == '__main__':
