2f4b5f1fa0e72a6f74410fe4b6a31ba7d3a9e64c
manoj7shekhawat <manoj7shekhawat@gmail.com>
2022-03-29 14:30:32 +0200

    master - Added solution for day 61

--- day-61/main.py
+++ day-61/main.py
@@ -1,16 +1,37 @@
 from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email, Length, email_validator
 
 app = Flask(__name__)
+app.secret_key = "any-string-you-want-just-keep-it-secret"
+Bootstrap(app)
+
+class LoginForm(FlaskForm):
+    email = StringField(label='Email', validators=[DataRequired(), Email()])
+    password = PasswordField(label='Password', validators=[DataRequired(), Length(min=6, message=f"Password should be minimum 6 characters long")])
+    submit = SubmitField(label='Log In')
 
 
 @app.route("/")
 def home():
     return render_template('index.html')
 
-@app.route("/login")
-def login():
-    return render_template("login.html")
 
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    login_form = LoginForm()
+    if login_form.validate_on_submit():
+        email = login_form.email.data
+        passwd = login_form.password.data
+        print(f"{email} {passwd}")
+        if email == "admin@email.com" and passwd == "12345678":
+            return render_template('success.html')
+        else:
+            return render_template('denied.html')
+
+    return render_template('login.html', form=login_form)
 
 if __name__ == '__main__':
     app.run(debug=True)
